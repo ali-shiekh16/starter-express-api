0948303b0f8bbabfe06ff87e0baab4543e48e588
Mohammad Ali <ali.shiekh16@outlook.com>
2023-07-23 02:27:49 +0500

    Add production configs.

---
+++ config/production.ts
@@ -0,0 +1,6 @@
+const config = {
+  name: 'Starter app',
+  port: process.env.PORT || 3000,
+};
+
+export default config;
--- index.ts
+++ index.ts
@@ -1,19 +1,19 @@
-import express from 'express';
-// import config from './config/default';
+import express, { Request, Response } from 'express';
 import config from 'config';
 
 const app = express();
 
-// const message = `${config.name} is running on port ${config.port}`;
 const message = `${config.get('name')} is running on port ${config.get(
   'port'
 )}`;
 
-// app.listen(config.port, () => {
+const environment = config.util.getEnv('NODE_ENV') || 'development';
+console.log('The environment is: ' + environment);
+
 app.listen(config.get('port'), () => {
   console.log(message);
 });
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send(message);
 });
