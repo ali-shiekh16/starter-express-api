8f076878c93e484fed30d20f8c4a460bd33b73c6
Mohammad Ali <ali.shiekh16@outlook.com>
2023-07-23 01:52:15 +0500

    Add config package again.

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
-import config from './config/default';
+// import config from './config/default';
+import config from 'config';
 
 const app = express();
 
-const message = `${config.name} is running on port ${config.port}`;
-app.listen(config.port, () => {
+// const message = `${config.name} is running on port ${config.port}`;
+const message = `${config.get('name')} is running on port ${config.get(
+  'port'
+)}`;
+
+// app.listen(config.port, () => {
+app.listen(config.get('port'), () => {
   console.log(message);
 });
 
