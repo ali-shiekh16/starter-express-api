18252b0d50bc8db7716bd9eb9517038207e5ae60
Mohammad Ali <ali.shiekh16@outlook.com>
2023-07-23 11:53:36 +0500

    Add config.

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 import express, { Request, Response } from 'express';
-// import config from 'config';
+import config from 'config';
 
 const app = express();
 
-// const message = `${config.get('name')} is running on port ${config.get(
-//   'port'
-// )}`;
-const message = 'Server is up and running :)';
+const message = `${config.get('name')} is running on port ${config.get(
+  'port'
+)}`;
 
 const environment = process.env.NODE_ENV || 'dev';
 console.log('The environment is: ' + environment);
